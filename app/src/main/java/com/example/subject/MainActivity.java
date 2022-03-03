eef3c7c0d8d271bd5200a57bf7fd224697a7ee67
superfer2002 <fporcel2002@gmail.com>
2022-03-03 19:10:09 +0100

    Main layout of main activity, some strings and images.

--- app/src/main/java/com/example/subject/MainActivity.java
+++ app/src/main/java/com/example/subject/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.subject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button subject1;
+    private Button subject2;
+    private Button subject3;
+
+    private void setButtons () {
+
+        /*subject1 = (ImageButton) findViewById(R.id.subject1);
+        subject1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        */
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setButtons();
     }
 }
