ee1c6bc80202338a5c8087635ea9810bcffab96d
superfer2002 <fporcel2002@gmail.com>
2022-03-05 18:19:11 +0100

    setButtons() and changeActivity() functions

--- app/src/main/java/com/example/subject/MainActivity.java
+++ app/src/main/java/com/example/subject/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.subject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,20 +13,57 @@ public class MainActivity extends AppCompatActivity {
     private Button subject2;
     private Button subject3;
 
+    private void changeActivity (int subjectNumber) {
+
+        Intent intent = new Intent(this, SubjectActivity.class);
+
+        switch (subjectNumber) {
+
+            case 1:
+                intent.putExtra("1", "1");
+                break;
+
+            case 2:
+                intent.putExtra("2", "2");
+                break;
+
+            case 3:
+                intent.putExtra("3", "3");
+                break;
+
+        }
+
+        startActivity(intent);
+
+    }
+
     private void setButtons () {
 
-        /*subject1 = (ImageButton) findViewById(R.id.subject1);
+        subject1 = (Button) findViewById(R.id.subject1);
         subject1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                changeActivity(1);
             }
         });
 
-        */
+        subject2 = (Button) findViewById(R.id.subject2);
+        subject2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity(2);
+            }
+        });
+
+        subject3 = (Button) findViewById(R.id.subject3);
+        subject3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity(3);
+            }
+        });
+
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
